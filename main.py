fb4fe6d3e3c8dfa06377cdfd3eef04ad2a3cd2d5
Miguel Angel Alvarez Clemente <michel_civic@hotmail.com>
2023-03-04 16:59:39 +0000

    Solo codigo en main, no funciona por error de OpenCV y camara incompatible en x64

--- main.py
+++ main.py
@@ -3,31 +3,55 @@ import requests
 import json
 from time import sleep
 from datetime import datetime
+import sys
 
-captura = cv2.VideoCapture(0)
-salida = cv2.VideoWriter('video1.avi',cv2.VideoWriter_fourcc(*'XVID'),20.0,(640,480))
-while (captura.isOpened()):
-    ret, imagen = captura.read()
-    if ret == True:
-        print('Procesando video...')
-        salida.write(imagen)
-        if cv2.waitKey(1) & 0xFF == ord('s'):
+ahora = datetime.now()
+fecha = ahora.strftime("%Y-%m-%d %H:%M:%S")
+
+
+UBICACION_FECHA_HORA = (0, 15)
+FUENTE_FECHA_Y_HORA = cv2.FONT_HERSHEY_PLAIN
+ESCALA_FUENTE = 1
+COLOR_FECHA_HORA = (255, 255, 255)
+GROSOR_TEXTO = 1
+TIPO_LINEA_TEXTO = cv2.LINE_AA
+FRAMES_VIDEO = 20.0
+RESOLUCION_VIDEO = (640, 480)
+salida = None
+captura =  cv2.VideoCapture(0)
+gst_str_rtp = "appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=192.168.86.26 port=5000"
+
+def agregar_fecha_hora_frame(frame):
+    cv2.putText(frame, fecha, UBICACION_FECHA_HORA, FUENTE_FECHA_Y_HORA,
+                ESCALA_FUENTE, COLOR_FECHA_HORA, GROSOR_TEXTO, TIPO_LINEA_TEXTO)
+
+def video_camara():
+    global salida
+    global captura
+    while True:
+        ret, frame = captura.read()
+        if ret:   
+            agregar_fecha_hora_frame(frame)
+            salida = cv2.VideoWriter(gst_str_rtp,0,FRAMES_VIDEO,RESOLUCION_VIDEO,True)
+            frame = cv2.flip(frame, 1)
+            salida.write(frame)
+            if cv2.waitKey(1) & 0xFF == ord('s'):
+                break
+        else:
+            print('---ERROR CAMARA---')
             break
-    else:
-        print('Error camara...')
-        break
+            '''
+            try:
+                enviar = requests.post('http://localhost:8000/upload', file='video1.avi',timeout=4)
+                print(f'Respuesta envio server {enviar}')
+            except (requests.exceptions.ConnectTimeout):
+                print('ERROR SERVER')
+                continue
+            '''
+    salida.release()
+    salida = None
+    captura.release() 
+    captura = None
 
-    
-    
-        '''
-        try:
-            enviar = requests.post('http://localhost:8000/upload', file='video1.avi',timeout=4)
-            print(f'Respuesta envio server {enviar}')
-        except (requests.exceptions.ConnectTimeout):
-            print('ERROR SERVER')
-            continue
-        '''
-        
-
-captura.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    video_camara()
