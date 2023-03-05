3211290c066dc544b4fdc397b1666c4e02fe9341
Miguel Angel Alvarez Clemente <michel_civic@hotmail.com>
2023-03-05 17:14:25 +0000

    Realizo video desde main y funciona OK. Pero no uso BBDD ni SERVER

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from time import sleep
 from datetime import datetime
 import sys
 
+
+print(f'Version OPENCV {cv2.__version__}')
+grabando= True
+n_frames = 0
+n_frames_max = 30
+list_frames =[]
 ahora = datetime.now()
 fecha = ahora.strftime("%Y-%m-%d %H:%M:%S")
-
+captura =  cv2.VideoCapture(0)
 
 UBICACION_FECHA_HORA = (0, 15)
 FUENTE_FECHA_Y_HORA = cv2.FONT_HERSHEY_PLAIN
@@ -15,43 +21,63 @@ ESCALA_FUENTE = 1
 COLOR_FECHA_HORA = (255, 255, 255)
 GROSOR_TEXTO = 1
 TIPO_LINEA_TEXTO = cv2.LINE_AA
-FRAMES_VIDEO = 20.0
-RESOLUCION_VIDEO = (640, 480)
+FRAMES_VIDEO = 30.0
+RESOLUCION_VIDEO = (int(captura.get(3)),int(captura.get(4)))
+print(f'Resolucion -> {RESOLUCION_VIDEO}')
+
 salida = None
-captura =  cv2.VideoCapture(0)
 gst_str_rtp = "appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=192.168.86.26 port=5000"
+salida = cv2.VideoWriter('video1.avi',cv2.VideoWriter_fourcc(*'XVID'),FRAMES_VIDEO,RESOLUCION_VIDEO)
+def guardar_video():
+    global salida
+    global captura
+    salida.release()
+    captura.release() 
+    print('--VIDEO GUARDADO--')
+    print(f'Se ha completado un total de {n_frames_max}, equivalente a {int(n_frames_max / FRAMES_VIDEO)} ')
+    print(f'Numero de frames totales : {n_frames} ...')
+    print(f'Longitud final en ListaFrames: {len(list_frames)} ...')
+        
+    
+    
+        
+
 
 def agregar_fecha_hora_frame(frame):
     cv2.putText(frame, fecha, UBICACION_FECHA_HORA, FUENTE_FECHA_Y_HORA,
                 ESCALA_FUENTE, COLOR_FECHA_HORA, GROSOR_TEXTO, TIPO_LINEA_TEXTO)
 
+
 def video_camara():
     global salida
     global captura
-    while True:
-        ret, frame = captura.read()
-        if ret:   
-            agregar_fecha_hora_frame(frame)
-            salida = cv2.VideoWriter(gst_str_rtp,0,FRAMES_VIDEO,RESOLUCION_VIDEO,True)
-            frame = cv2.flip(frame, 1)
-            salida.write(frame)
-            if cv2.waitKey(1) & 0xFF == ord('s'):
-                break
+    global grabando
+    global n_frames
+    global n_frames_max
+    n_frames_max = int(FRAMES_VIDEO) * int(input('Indica segundos de duracion:\n'))
+    while n_frames != n_frames_max:
+        print(f'Frames a reproducir: {n_frames_max}\n')
+        print(f'Estado de camara: {grabando} ...')
+        if grabando:
+            print('GRABANDO VIDEO...')
+            ret, frame = captura.read()
+            if ret:
+                print('Captura OK...')   
+                agregar_fecha_hora_frame(frame)
+                frame = cv2.flip(frame, 1)
+                list_frames.append(frame)
+                n_frames = n_frames + 1
+                print(f'Numero de frame: {n_frames} ...')
+                print(f'Contenido ListaFrames: {len(list_frames)} ...')
+            else:
+                print('---ERROR CAMARA---')
         else:
-            print('---ERROR CAMARA---')
-            break
-            '''
-            try:
-                enviar = requests.post('http://localhost:8000/upload', file='video1.avi',timeout=4)
-                print(f'Respuesta envio server {enviar}')
-            except (requests.exceptions.ConnectTimeout):
-                print('ERROR SERVER')
-                continue
-            '''
-    salida.release()
-    salida = None
-    captura.release() 
-    captura = None
+            print('---NO ESTA GRABANDO---')
+    if n_frames == n_frames_max:
+        for i in range(len(list_frames)):
+            salida.write(list_frames[i])
+        guardar_video()
 
 if __name__ == "__main__":
     video_camara()
+    
