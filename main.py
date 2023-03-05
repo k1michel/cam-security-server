4876408988f0255f108a9801010cc22ee836e782
Miguel Angel Alvarez Clemente <michel_civic@hotmail.com>
2023-03-05 20:07:15 +0000

    Funciona la grabacion y el envio al server en misma red con distinta IP

--- main.py
+++ main.py
@@ -12,31 +12,51 @@ n_frames = 0
 n_frames_max = 30
 list_frames =[]
 ahora = datetime.now()
-fecha = ahora.strftime("%Y-%m-%d %H:%M:%S")
+fecha = ahora.strftime("%Y-%m-%d_%H-%M-%S")
 captura =  cv2.VideoCapture(0)
 
-UBICACION_FECHA_HORA = (0, 15)
+UBICACION_FECHA_HORA = (20, 40)
 FUENTE_FECHA_Y_HORA = cv2.FONT_HERSHEY_PLAIN
-ESCALA_FUENTE = 1
+ESCALA_FUENTE = 2
 COLOR_FECHA_HORA = (255, 255, 255)
 GROSOR_TEXTO = 1
 TIPO_LINEA_TEXTO = cv2.LINE_AA
 FRAMES_VIDEO = 30.0
-RESOLUCION_VIDEO = (int(captura.get(3)),int(captura.get(4)))
+escala = 50
+captura_ancho = int(int(captura.get(3))*escala/100)
+captura_alto = int(int(captura.get(4))*escala/100)
+RESOLUCION_VIDEO = (captura_ancho,captura_alto)#(int(captura.get(3)),int(captura.get(4)))
 print(f'Resolucion -> {RESOLUCION_VIDEO}')
 
+url = 'http://192.168.1.2:8000/upload'
+nombre_video = f'video_{fecha}.avi'
 salida = None
 gst_str_rtp = "appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=192.168.86.26 port=5000"
-salida = cv2.VideoWriter('video1.avi',cv2.VideoWriter_fourcc(*'XVID'),FRAMES_VIDEO,RESOLUCION_VIDEO)
+salida = cv2.VideoWriter(nombre_video,cv2.VideoWriter_fourcc(*'XVID'),FRAMES_VIDEO,RESOLUCION_VIDEO)
+
+
 def guardar_video():
     global salida
     global captura
+
     salida.release()
     captura.release() 
     print('--VIDEO GUARDADO--')
     print(f'Se ha completado un total de {n_frames_max}, equivalente a {int(n_frames_max / FRAMES_VIDEO)} ')
     print(f'Numero de frames totales : {n_frames} ...')
     print(f'Longitud final en ListaFrames: {len(list_frames)} ...')
+    print('Preparando envio...')
+    enviar_video()
+
+def enviar_video():
+    global url
+    files = {'file': open(nombre_video, 'rb')}
+    response = requests.post(url, files=files)  
+    print('-- Video enviado --')
+    if str(response) == '<Response [200]>':
+        print('-- Video recibido en Server OK --')
+    else:
+        print(f'---Error en server: {response} ---')
         
     
     
@@ -54,7 +74,9 @@ def video_camara():
     global grabando
     global n_frames
     global n_frames_max
+
     n_frames_max = int(FRAMES_VIDEO) * int(input('Indica segundos de duracion:\n'))
+
     while n_frames != n_frames_max:
         print(f'Frames a reproducir: {n_frames_max}\n')
         print(f'Estado de camara: {grabando} ...')
@@ -63,9 +85,13 @@ def video_camara():
             ret, frame = captura.read()
             if ret:
                 print('Captura OK...')   
-                agregar_fecha_hora_frame(frame)
-                frame = cv2.flip(frame, 1)
-                list_frames.append(frame)
+                ancho = int(frame.shape[1]*escala/100)
+                alto = int(frame.shape[0]*escala/100)
+                print(f'Dimensiones escaladas: ancho={ancho} x alto={alto}')
+                frame_escala = cv2.resize(frame,(ancho,alto),interpolation=cv2.INTER_AREA)
+                agregar_fecha_hora_frame(frame_escala)
+                #frame_escala = cv2.flip(frame_escala, 1)
+                list_frames.append(frame_escala)
                 n_frames = n_frames + 1
                 print(f'Numero de frame: {n_frames} ...')
                 print(f'Contenido ListaFrames: {len(list_frames)} ...')
@@ -77,6 +103,7 @@ def video_camara():
         for i in range(len(list_frames)):
             salida.write(list_frames[i])
         guardar_video()
+
 
 if __name__ == "__main__":
     video_camara()
